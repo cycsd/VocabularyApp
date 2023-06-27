1251e665a1e8ec611a415bfa815be947287497dc
cycsd <cycsd9211@gmail.com>
2023-06-28 01:06:28 +0800

    加入webAPI

---
+++ VocabularyAPI/Controllers/ArticleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.SearchService;
+using ServiceLayer.SearchService.Concrete;
+
+namespace VocabularyAPI.Controllers
+{
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        [HttpGet()]
+        public IEnumerable<ArticleDto> GetArticle()
+        {
+            var serv = new ArticleService();
+            var articeList = serv.GetArticles();
+            return articeList;
+        }
+
+    }
+}
